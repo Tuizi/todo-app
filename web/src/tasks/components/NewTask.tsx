015c8a9814d711e5b7cc4794dfb22f486cba8c59
Fabien Rogeret <fabien@sweetiq.com>
2020-01-30 23:41:05 -0500

    Send the right text

--- web/src/tasks/components/NewTask.tsx
+++ web/src/tasks/components/NewTask.tsx
@@ -1,20 +1,26 @@
-import React, { FormEvent } from 'react';
 import TextField from '@material-ui/core/TextField';
+import React, { FormEvent } from 'react';
 import { useDispatch } from 'react-redux';
 import tasksStore from '../store';
 
 const NewTask = () => {
   const dispatch = useDispatch();
+  let text = '';
 
   const handleFormSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    dispatch(tasksStore.actions.newTask('toto'));
+    dispatch(tasksStore.actions.newTask(text));
   };
 
   return (
     <form onSubmit={handleFormSubmit}>
-      <TextField required fullWidth label='Enter a new task...' />
+      <TextField
+        required
+        fullWidth
+        label='Enter a new task...'
+        onChange={e => (text = e.target.value)}
+      />
     </form>
   );
 };
